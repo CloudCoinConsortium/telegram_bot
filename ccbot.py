4b6cc2ba1c1bed73e140456901858c2f3889e14e
Navraj Singh <navraj@outlook.com>
2023-01-04 09:41:22 +1100

    Added help command

--- ccbot.py
+++ ccbot.py
@@ -1,35 +1,53 @@
 import telegram
-from telegram.ext import Updater, CommandHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from help import help
 
 def command(update, context):
-    # Check if the message has an attachment
-    if update.message.document:
-        # Get the attachment
-        attachment = update.message.document
-        file_id = attachment.file_id
-        file_name = attachment.file_name
-        file_size = attachment.file_size
+    # Get the user's message
+    message = update.message.text
+    
+    # Split the message into words
+    print(update.message)
+    words = ['echo']
+    
+    # Get the first word (the command)
+    command = words[0]
+    
+    # Get the rest of the words (the arguments)
+    args = words[1:]
+    
+    # Check if the message has an image attachment
+    if update.message.photo:
+        # Get the largest image in the list of images
+        image = update.message.photo[-1]
+        file_id = image.file_id
+        file_size = image.file_size
         
-        # Do something with the attachment
-        # For example, you could download the file and save it to disk
+        # Download the image
         file = context.bot.get_file(file_id)
-        file.download("/path/to/save/file/{}".format(file_name))
+        file.download("image.jpg")
         
-        update.message.reply_text("Received attachment with file name {} and size {}".format(file_name, file_size))
-    else:
-        update.message.reply_text("No attachment received")
+        # Send a message with the image's file size
+        update.message.reply_text("Received image with size {}".format(file_size))
+    
+    # Send a message with the command and arguments
+    update.message.reply_text("You used the {} command with the following arguments: {}".format(command, args))
 
 def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("5737629326:AAEpF4k5XrWzniyuS3wfawgQYYUcTvRKLsA", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Add a handler to handle the command
-    dp.add_handler(CommandHandler("command", command))
+    # Add a command handler to handle the /echo command
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("echo", command))
+
+    # Add a message handler to handle messages with image attachments
+    dp.add_handler(MessageHandler(Filters.photo, command))
 
     # Start the Bot
     updater.start_polling()
